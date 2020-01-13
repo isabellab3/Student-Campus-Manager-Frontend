b3ee50dcc02f158b522983bc0214a5464f19d4bd
Samson <samson50wu@gmail.com>
2020-01-13 18:42:02 -0500

    Added campuses dummy data

---
+++ src/dummyData/campusesData.js
@@ -0,0 +1,93 @@
+const allCampuses = [
+  {
+    id: 0,
+    campusName: "Warwick University",
+    address1: "67-64 Babbage Boulevard",
+    address2: "Coconutwood, WY 17611",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 1,
+    campusName: "Stossel University",
+    address1: "152 Obama Drive",
+    address2: "Tortoiseheart, KY 59593",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 2,
+    campusName: "Stewart Bible College",
+    address1: "64-93 Columbus Loop",
+    address2: "Westston, CA 06674",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 3,
+    campusName: "Drever University of Science and Technology",
+    address1: "76-22 Oyster Lane",
+    address2: "Oldhold, PA 64042",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 4,
+    campusName: "Mendelman University of Science and Technology",
+    address1: "685-83 Bowe Mews",
+    address2: "Winterhold, VT 37845",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 5,
+    campusName: "Canaan College",
+    address1: "56-94 Herring Way",
+    address2: "Silver Gardens, ND 04576",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 6,
+    campusName: "Greendale University",
+    address1: "47-12 Cardinal Way",
+    address2: "Elmhold, CO 42877",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 7,
+    campusName: "Tooman College",
+    address1: "27-07 Cardinal Terrace",
+    address2: "Oldmarch, FL 56414",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 8,
+    campusName: "Warwick Technical College",
+    address1: "839 Aardvark Road",
+    address2: "Pine Hills, OH 31194",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  },
+
+  {
+    id: 9,
+    campusName: "Padron University",
+    address1: "922-75 Ventmor Place",
+    address2: "Pineapple Canyon, LA 26556",
+    campusImage: "/images/campuses/campus01.jpg",
+    description: "<campus description goes here>"
+  }
+];
+
+export default allCampuses;
