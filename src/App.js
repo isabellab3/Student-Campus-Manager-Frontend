d56a4658dc1a57caabe7117a247e0fd9dbed0c04
Eva Yan <eyan0749@gmail.com>
2020-01-13 23:41:29 -0500

    Added navbar and basic link functionality, as well as styles.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import './App.css';
 
 // these will be added to the barrel file index.js in utilities
 // import SingleCampus from "./components";
@@ -17,8 +18,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>(Navbar goes here)</div>
-        {/* It doesn't seem to recognize <navbar> */}
+        <div className="navbar">
+          <h1>Student Campus Manager</h1>
+          <div className="navbar-links">
+            <Link to="/">Home</Link>
+            <Link to="/campuses">Campuses</Link> 
+            <Link to="/addstudent">Add Student</Link>
+          </div>
+        </div>
         <Switch>
           <Route exact path="/campuses" component={DisplayCampusesContainer} />
           {/* <Route exact path="/student/:id" component={SingleCampus} /> */}
