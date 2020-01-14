e03a4b5050da8e781a72c4cf3dbe8460ecf7f438
Samson <samson50wu@gmail.com>
2020-01-14 11:00:55 -0500

    Added dummy data for 10 campuses and 100 students

--- src/dummyData/studentsData.js
+++ src/dummyData/studentsData.js
@@ -1,22 +1,904 @@
 const allStudents = [
-  {
+{
+    id: 0,
+    firstName: "Ishan",
+    lastName: "Snyder",
+    gpa: 4.0,
+    image: "user18.jpg",
+    email: "IshanSnyder50@angelfire.com",
+},
+
+{
     id: 1,
-    firstName: "Harry",
-    lastName: "Potter",
-    gpa: 2.9,
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/daniel-radcliffe-harry-potter-1577468480.jpg?crop=0.871xw:0.776xh;0.0425xw,0&resize=480:*",
-    email:  "harrypotter@hogwarts.edu",
-    campus: "Hogwarts"
-  },
-  {
+    firstName: "Sanvi",
+    lastName: "Bartik",
+    gpa: 3.9,
+    image: "user01.jpg",
+    email: "SanviBartik32@orange.com",
+},
+
+{
     id: 2,
-    firstName: "First",
-    lastName: "Last",
+    firstName: "Ali",
+    lastName: "Jones",
+    gpa: 3.3,
+    image: "user28.jpg",
+    email: "AliJones48@angelfire.com",
+},
+
+{
+    id: 3,
+    firstName: "Tamar",
+    lastName: "Hopper",
+    gpa: 3.6,
+    image: "user11.jpg",
+    email: "TamarHopper45@aol.com",
+},
+
+{
+    id: 4,
+    firstName: "Noah",
+    lastName: "Miller",
+    gpa: 3.1,
+    image: "user15.jpg",
+    email: "NoahMiller31@myspace.com",
+},
+
+{
+    id: 5,
+    firstName: "Aarav",
+    lastName: "Novik",
+    gpa: 3.4,
+    image: "user20.jpg",
+    email: "AaravNovik01@angelfire.com",
+},
+
+{
+    id: 6,
+    firstName: "Arnav",
+    lastName: "McClintock",
+    gpa: 3.4,
+    image: "user18.jpg",
+    email: "ArnavMcClintock33@orange.com",
+},
+
+{
+    id: 7,
+    firstName: "Mathias",
+    lastName: "Dahan",
+    gpa: 3.7,
+    image: "user14.jpg",
+    email: "MathiasDahan62@gmail.com",
+},
+
+{
+    id: 8,
+    firstName: "Zofia",
+    lastName: "Rebane",
+    gpa: 3.7,
+    image: "user01.jpg",
+    email: "ZofiaRebane97@geocities.com",
+},
+
+{
+    id: 9,
+    firstName: "Youssef",
+    lastName: "Lichterman",
     gpa: 3.5,
-    image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/daniel-radcliffe-harry-potter-1577468480.jpg?crop=0.871xw:0.776xh;0.0425xw,0&resize=480:*",
-    email: "First.Last.22@myhunter.cuny.edu",
-    campus: "Hunter College"
-  }
+    image: "user04.jpg",
+    email: "YoussefLichterman90@msn.com",
+},
+
+{
+    id: 10,
+    firstName: "Arttu",
+    lastName: "Babbage",
+    gpa: 3.3,
+    image: "user19.jpg",
+    email: "ArttuBabbage39@msn.com",
+},
+
+{
+    id: 11,
+    firstName: "Cornelius",
+    lastName: "Bhoumik",
+    gpa: 3.5,
+    image: "user25.jpg",
+    email: "CorneliusBhoumik78@outlook.com",
+},
+
+{
+    id: 12,
+    firstName: "Ali",
+    lastName: "McNulty",
+    gpa: 3.3,
+    image: "user04.jpg",
+    email: "AliMcNulty73@outlook.com",
+},
+
+{
+    id: 13,
+    firstName: "Felix",
+    lastName: "Tsui",
+    gpa: 3.9,
+    image: "user02.jpg",
+    email: "FelixTsui02@angelfire.com",
+},
+
+{
+    id: 14,
+    firstName: "Yekaterina",
+    lastName: "Snyder",
+    gpa: 3.2,
+    image: "user12.jpg",
+    email: "YekaterinaSnyder66@hotmail.com",
+},
+
+{
+    id: 15,
+    firstName: "Hinata",
+    lastName: "Rebane",
+    gpa: 3.3,
+    image: "user15.jpg",
+    email: "HinataRebane26@wanadoo.com",
+},
+
+{
+    id: 16,
+    firstName: "Lucas",
+    lastName: "McClintock",
+    gpa: 3.3,
+    image: "user13.jpg",
+    email: "LucasMcClintock86@myspace.com",
+},
+
+{
+    id: 17,
+    firstName: "Ishan",
+    lastName: "Babbage",
+    gpa: 3.9,
+    image: "user16.jpg",
+    email: "IshanBabbage36@geocities.com",
+},
+
+{
+    id: 18,
+    firstName: "Devansh",
+    lastName: "Tsui",
+    gpa: 3.3,
+    image: "user10.jpg",
+    email: "DevanshTsui43@geocities.com",
+},
+
+{
+    id: 19,
+    firstName: "Amina",
+    lastName: "Singh",
+    gpa: 3.3,
+    image: "user06.jpg",
+    email: "AminaSingh19@msn.com",
+},
+
+{
+    id: 20,
+    firstName: "Clara",
+    lastName: "Kenobi",
+    gpa: 3.1,
+    image: "user07.jpg",
+    email: "ClaraKenobi67@orange.com",
+},
+
+{
+    id: 21,
+    firstName: "Ishan",
+    lastName: "McNulty",
+    gpa: 3.9,
+    image: "user18.jpg",
+    email: "IshanMcNulty23@wanadoo.com",
+},
+
+{
+    id: 22,
+    firstName: "Rowena",
+    lastName: "Marquez",
+    gpa: 3.8,
+    image: "user19.jpg",
+    email: "RowenaMarquez13@facebook.com",
+},
+
+{
+    id: 23,
+    firstName: "Mohammad",
+    lastName: "Wong",
+    gpa: 3.6,
+    image: "user06.jpg",
+    email: "MohammadWong78@wanadoo.com",
+},
+
+{
+    id: 24,
+    firstName: "Arnav",
+    lastName: "Beaumont",
+    gpa: 3.1,
+    image: "user13.jpg",
+    email: "ArnavBeaumont84@geocities.com",
+},
+
+{
+    id: 25,
+    firstName: "Alvaro",
+    lastName: "Demir",
+    gpa: 3.3,
+    image: "user18.jpg",
+    email: "AlvaroDemir44@wanadoo.com",
+},
+
+{
+    id: 26,
+    firstName: "Rowena",
+    lastName: "Dahan",
+    gpa: 3.2,
+    image: "user13.jpg",
+    email: "RowenaDahan75@aol.com",
+},
+
+{
+    id: 27,
+    firstName: "Maximilian",
+    lastName: "Walker",
+    gpa: 3.2,
+    image: "user05.jpg",
+    email: "MaximilianWalker96@myspace.com",
+},
+
+{
+    id: 28,
+    firstName: "Marek",
+    lastName: "Kuznetsov",
+    gpa: 3.3,
+    image: "user25.jpg",
+    email: "MarekKuznetsov09@orange.com",
+},
+
+{
+    id: 29,
+    firstName: "Barack",
+    lastName: "Demir",
+    gpa: 3.4,
+    image: "user24.jpg",
+    email: "BarackDemir88@outlook.com",
+},
+
+{
+    id: 30,
+    firstName: "Lucas",
+    lastName: "Peeters",
+    gpa: 3.1,
+    image: "user15.jpg",
+    email: "LucasPeeters18@myspace.com",
+},
+
+{
+    id: 31,
+    firstName: "Amelia",
+    lastName: "Chen",
+    gpa: 3.9,
+    image: "user28.jpg",
+    email: "AmeliaChen45@geocities.com",
+},
+
+{
+    id: 32,
+    firstName: "Oliver",
+    lastName: "Novik",
+    gpa: 3.9,
+    image: "user13.jpg",
+    email: "OliverNovik97@angelfire.com",
+},
+
+{
+    id: 33,
+    firstName: "Hinata",
+    lastName: "Williams",
+    gpa: 3.3,
+    image: "user22.jpg",
+    email: "HinataWilliams57@wanadoo.com",
+},
+
+{
+    id: 34,
+    firstName: "Alvaro",
+    lastName: "Bhoumik",
+    gpa: 3.2,
+    image: "user02.jpg",
+    email: "AlvaroBhoumik29@msn.com",
+},
+
+{
+    id: 35,
+    firstName: "Marek",
+    lastName: "Novik",
+    gpa: 3.2,
+    image: "user11.jpg",
+    email: "MarekNovik33@yahoo.com",
+},
+
+{
+    id: 36,
+    firstName: "Ines",
+    lastName: "Kuznetsov",
+    gpa: 3.5,
+    image: "user24.jpg",
+    email: "InesKuznetsov13@aol.com",
+},
+
+{
+    id: 37,
+    firstName: "Camille",
+    lastName: "Peeters",
+    gpa: 3.2,
+    image: "user23.jpg",
+    email: "CamillePeeters56@myspace.com",
+},
+
+{
+    id: 38,
+    firstName: "Arttu",
+    lastName: "Kenobi",
+    gpa: 3.7,
+    image: "user08.jpg",
+    email: "ArttuKenobi90@orange.com",
+},
+
+{
+    id: 39,
+    firstName: "Cristobal",
+    lastName: "Kuznetsov",
+    gpa: 3.9,
+    image: "user28.jpg",
+    email: "CristobalKuznetsov01@angelfire.com",
+},
+
+{
+    id: 40,
+    firstName: "Rowena",
+    lastName: "Beaumont",
+    gpa: 3.7,
+    image: "user19.jpg",
+    email: "RowenaBeaumont01@msn.com",
+},
+
+{
+    id: 41,
+    firstName: "Rowena",
+    lastName: "Holberton",
+    gpa: 3.6,
+    image: "user28.jpg",
+    email: "RowenaHolberton92@orange.com",
+},
+
+{
+    id: 42,
+    firstName: "Lucas",
+    lastName: "Bhoumik",
+    gpa: 3.9,
+    image: "user10.jpg",
+    email: "LucasBhoumik83@gmail.com",
+},
+
+{
+    id: 43,
+    firstName: "Felix",
+    lastName: "Wescoff",
+    gpa: 3.6,
+    image: "user16.jpg",
+    email: "FelixWescoff33@gmail.com",
+},
+
+{
+    id: 44,
+    firstName: "Fatima",
+    lastName: "Bhoumik",
+    gpa: 3.1,
+    image: "user24.jpg",
+    email: "FatimaBhoumik70@hotmail.com",
+},
+
+{
+    id: 45,
+    firstName: "Miriam",
+    lastName: "Davis",
+    gpa: 3.8,
+    image: "user07.jpg",
+    email: "MiriamDavis17@msn.com",
+},
+
+{
+    id: 46,
+    firstName: "Camille",
+    lastName: "Moore",
+    gpa: 3.1,
+    image: "user02.jpg",
+    email: "CamilleMoore02@yahoo.com",
+},
+
+{
+    id: 47,
+    firstName: "Marta",
+    lastName: "Walker",
+    gpa: 3.1,
+    image: "user23.jpg",
+    email: "MartaWalker64@outlook.com",
+},
+
+{
+    id: 48,
+    firstName: "Ishan",
+    lastName: "Singh",
+    gpa: 3.5,
+    image: "user13.jpg",
+    email: "IshanSingh96@yahoo.com",
+},
+
+{
+    id: 49,
+    firstName: "Youssef",
+    lastName: "Singh",
+    gpa: 3.8,
+    image: "user28.jpg",
+    email: "YoussefSingh16@gmail.com",
+},
+
+{
+    id: 50,
+    firstName: "Arnav",
+    lastName: "Wescoff",
+    gpa: 3.2,
+    image: "user04.jpg",
+    email: "ArnavWescoff23@myspace.com",
+},
+
+{
+    id: 51,
+    firstName: "Tamar",
+    lastName: "Wagner",
+    gpa: 3.3,
+    image: "user09.jpg",
+    email: "TamarWagner93@outlook.com",
+},
+
+{
+    id: 52,
+    firstName: "Tatiana",
+    lastName: "Wagner",
+    gpa: 3.7,
+    image: "user07.jpg",
+    email: "TatianaWagner48@aol.com",
+},
+
+{
+    id: 53,
+    firstName: "Ivan",
+    lastName: "Suzuki",
+    gpa: 3.6,
+    image: "user26.jpg",
+    email: "IvanSuzuki78@orange.com",
+},
+
+{
+    id: 54,
+    firstName: "Enzo",
+    lastName: "Novik",
+    gpa: 3.5,
+    image: "user21.jpg",
+    email: "EnzoNovik02@wanadoo.com",
+},
+
+{
+    id: 55,
+    firstName: "Tomasz",
+    lastName: "Walker",
+    gpa: 3.8,
+    image: "user21.jpg",
+    email: "TomaszWalker37@wanadoo.com",
+},
+
+{
+    id: 56,
+    firstName: "Aditya",
+    lastName: "Kamau",
+    gpa: 3.6,
+    image: "user12.jpg",
+    email: "AdityaKamau90@geocities.com",
+},
+
+{
+    id: 57,
+    firstName: "Amina",
+    lastName: "Ryan",
+    gpa: 3.2,
+    image: "user30.jpg",
+    email: "AminaRyan88@yahoo.com",
+},
+
+{
+    id: 58,
+    firstName: "Sofia",
+    lastName: "Novik",
+    gpa: 4.0,
+    image: "user07.jpg",
+    email: "SofiaNovik98@facebook.com",
+},
+
+{
+    id: 59,
+    firstName: "Cornelius",
+    lastName: "Peeters",
+    gpa: 4.0,
+    image: "user20.jpg",
+    email: "CorneliusPeeters30@msn.com",
+},
+
+{
+    id: 60,
+    firstName: "Jade",
+    lastName: "Kumara",
+    gpa: 3.6,
+    image: "user03.jpg",
+    email: "JadeKumara48@hotmail.com",
+},
+
+{
+    id: 61,
+    firstName: "Maximilian",
+    lastName: "Wagner",
+    gpa: 3.4,
+    image: "user12.jpg",
+    email: "MaximilianWagner77@hotmail.com",
+},
+
+{
+    id: 62,
+    firstName: "Noah",
+    lastName: "Kumara",
+    gpa: 3.5,
+    image: "user04.jpg",
+    email: "NoahKumara57@msn.com",
+},
+
+{
+    id: 63,
+    firstName: "Cornelius",
+    lastName: "Singh",
+    gpa: 3.1,
+    image: "user19.jpg",
+    email: "CorneliusSingh29@myspace.com",
+},
+
+{
+    id: 64,
+    firstName: "Mohammad",
+    lastName: "Garcia",
+    gpa: 4.0,
+    image: "user12.jpg",
+    email: "MohammadGarcia41@msn.com",
+},
+
+{
+    id: 65,
+    firstName: "Oliver",
+    lastName: "Kumara",
+    gpa: 3.5,
+    image: "user10.jpg",
+    email: "OliverKumara14@facebook.com",
+},
+
+{
+    id: 66,
+    firstName: "Rowena",
+    lastName: "Peeters",
+    gpa: 4.0,
+    image: "user04.jpg",
+    email: "RowenaPeeters94@orange.com",
+},
+
+{
+    id: 67,
+    firstName: "Javiera",
+    lastName: "Lovelace",
+    gpa: 3.9,
+    image: "user10.jpg",
+    email: "JavieraLovelace94@facebook.com",
+},
+
+{
+    id: 68,
+    firstName: "Oscar",
+    lastName: "Beaumont",
+    gpa: 4.0,
+    image: "user03.jpg",
+    email: "OscarBeaumont90@geocities.com",
+},
+
+{
+    id: 69,
+    firstName: "Hinata",
+    lastName: "Bartik",
+    gpa: 3.5,
+    image: "user27.jpg",
+    email: "HinataBartik49@gmail.com",
+},
+
+{
+    id: 70,
+    firstName: "Ali",
+    lastName: "Garcia",
+    gpa: 3.3,
+    image: "user08.jpg",
+    email: "AliGarcia06@wanadoo.com",
+},
+
+{
+    id: 71,
+    firstName: "Tamar",
+    lastName: "Yamamoto",
+    gpa: 3.3,
+    image: "user20.jpg",
+    email: "TamarYamamoto77@geocities.com",
+},
+
+{
+    id: 72,
+    firstName: "Camille",
+    lastName: "Marquez",
+    gpa: 3.8,
+    image: "user12.jpg",
+    email: "CamilleMarquez55@orange.com",
+},
+
+{
+    id: 73,
+    firstName: "Arttu",
+    lastName: "Kamau",
+    gpa: 4.0,
+    image: "user03.jpg",
+    email: "ArttuKamau77@orange.com",
+},
+
+{
+    id: 74,
+    firstName: "Ali",
+    lastName: "Lovelace",
+    gpa: 3.5,
+    image: "user27.jpg",
+    email: "AliLovelace13@orange.com",
+},
+
+{
+    id: 75,
+    firstName: "Arttu",
+    lastName: "Kumara",
+    gpa: 4.0,
+    image: "user30.jpg",
+    email: "ArttuKumara30@myspace.com",
+},
+
+{
+    id: 76,
+    firstName: "Cristobal",
+    lastName: "Bartik",
+    gpa: 4.0,
+    image: "user13.jpg",
+    email: "CristobalBartik78@myspace.com",
+},
+
+{
+    id: 77,
+    firstName: "Youssef",
+    lastName: "Wong",
+    gpa: 3.2,
+    image: "user01.jpg",
+    email: "YoussefWong95@msn.com",
+},
+
+{
+    id: 78,
+    firstName: "Sofia",
+    lastName: "Kuznetsov",
+    gpa: 3.7,
+    image: "user11.jpg",
+    email: "SofiaKuznetsov16@outlook.com",
+},
+
+{
+    id: 79,
+    firstName: "Harini",
+    lastName: "Rodriguez",
+    gpa: 4.0,
+    image: "user11.jpg",
+    email: "HariniRodriguez44@geocities.com",
+},
+
+{
+    id: 80,
+    firstName: "Marek",
+    lastName: "Babbage",
+    gpa: 3.9,
+    image: "user08.jpg",
+    email: "MarekBabbage49@aol.com",
+},
+
+{
+    id: 81,
+    firstName: "Tatiana",
+    lastName: "Cho",
+    gpa: 3.9,
+    image: "user24.jpg",
+    email: "TatianaCho58@gmail.com",
+},
+
+{
+    id: 82,
+    firstName: "Maximilian",
+    lastName: "Garcia",
+    gpa: 3.2,
+    image: "user03.jpg",
+    email: "MaximilianGarcia53@msn.com",
+},
+
+{
+    id: 83,
+    firstName: "Lucas",
+    lastName: "Walker",
+    gpa: 3.2,
+    image: "user08.jpg",
+    email: "LucasWalker11@geocities.com",
+},
+
+{
+    id: 84,
+    firstName: "Tamar",
+    lastName: "Dahan",
+    gpa: 3.5,
+    image: "user15.jpg",
+    email: "TamarDahan62@aol.com",
+},
+
+{
+    id: 85,
+    firstName: "Lucas",
+    lastName: "Jones",
+    gpa: 3.6,
+    image: "user29.jpg",
+    email: "LucasJones09@geocities.com",
+},
+
+{
+    id: 86,
+    firstName: "Mathias",
+    lastName: "Novik",
+    gpa: 3.8,
+    image: "user05.jpg",
+    email: "MathiasNovik74@wanadoo.com",
+},
+
+{
+    id: 87,
+    firstName: "Sofia",
+    lastName: "Novik",
+    gpa: 3.1,
+    image: "user02.jpg",
+    email: "SofiaNovik31@geocities.com",
+},
+
+{
+    id: 88,
+    firstName: "Harini",
+    lastName: "Demir",
+    gpa: 3.7,
+    image: "user04.jpg",
+    email: "HariniDemir07@myspace.com",
+},
+
+{
+    id: 89,
+    firstName: "Ridhi",
+    lastName: "Beaumont",
+    gpa: 3.8,
+    image: "user14.jpg",
+    email: "RidhiBeaumont49@outlook.com",
+},
+
+{
+    id: 90,
+    firstName: "Zofia",
+    lastName: "Hasan",
+    gpa: 3.3,
+    image: "user16.jpg",
+    email: "ZofiaHasan54@angelfire.com",
+},
+
+{
+    id: 91,
+    firstName: "Fatima",
+    lastName: "Suzuki",
+    gpa: 3.2,
+    image: "user13.jpg",
+    email: "FatimaSuzuki53@facebook.com",
+},
+
+{
+    id: 92,
+    firstName: "Ishan",
+    lastName: "Yamamoto",
+    gpa: 3.1,
+    image: "user06.jpg",
+    email: "IshanYamamoto46@myspace.com",
+},
+
+{
+    id: 93,
+    firstName: "Maximilian",
+    lastName: "Harris",
+    gpa: 3.8,
+    image: "user05.jpg",
+    email: "MaximilianHarris93@hotmail.com",
+},
+
+{
+    id: 94,
+    firstName: "Ava",
+    lastName: "Davis",
+    gpa: 3.4,
+    image: "user22.jpg",
+    email: "AvaDavis44@hotmail.com",
+},
+
+{
+    id: 95,
+    firstName: "Zofia",
+    lastName: "Novik",
+    gpa: 3.7,
+    image: "user20.jpg",
+    email: "ZofiaNovik74@msn.com",
+},
+
+{
+    id: 96,
+    firstName: "Marek",
+    lastName: "Dahan",
+    gpa: 3.2,
+    image: "user14.jpg",
+    email: "MarekDahan84@myspace.com",
+},
+
+{
+    id: 97,
+    firstName: "Arttu",
+    lastName: "Walker",
+    gpa: 3.8,
+    image: "user16.jpg",
+    email: "ArttuWalker42@outlook.com",
+},
+
+{
+    id: 98,
+    firstName: "Jade",
+    lastName: "Burnell",
+    gpa: 3.4,
+    image: "user22.jpg",
+    email: "JadeBurnell67@wanadoo.com",
+},
+
+{
+    id: 99,
+    firstName: "Yekaterina",
+    lastName: "Wagner",
+    gpa: 4.0,
+    image: "user18.jpg",
+    email: "YekaterinaWagner10@aol.com",
+},
+
 ]
 
 export default allStudents
