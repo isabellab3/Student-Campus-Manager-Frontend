bd7bc1fbe3ab1d291514b017ca5771ff9f5c219f
Darren Zhang <darrenzhang200027@gmail.com>
2020-01-14 12:23:55 -0500

    changed student card proptypes

--- src/components/views/StudentCard.jsx
+++ src/components/views/StudentCard.jsx
@@ -3,25 +3,27 @@ import PropTypes from "prop-types"
 import '../views/StudentCard.css'
 
 const StudentCard = props => {
+  console.log(props);
   return (
     <div className='student-card'>
-      <img src={props.image} alt="" />
+      <img src={props.student.image} alt="" />
       <h3>
-        {props.firstName} {props.lastName}
+        {props.student.firstName} {props.student.lastName}
       </h3>
-      <h4>{props.campus}</h4>
+      <h4>{props.student.campus}</h4>
       <button onClick={props.onClick}>{props.buttonText}</button>
     </div>
   )
 }
 
 StudentCard.propTypes = {
-  image: PropTypes.string.isRequired,
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  campus: PropTypes.string.isRequired,
+  // image: PropTypes.string.isRequired,
+  // firstName: PropTypes.string.isRequired,
+  // lastName: PropTypes.string.isRequired,
+  // campus: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
-  buttonText: PropTypes.string.isRequired
+  buttonText: PropTypes.string.isRequired,
+  student: PropTypes.object.isRequired
 }
 
 export default StudentCard
