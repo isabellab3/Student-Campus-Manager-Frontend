6dcbd81ced781d3ddb20f2326f2bb0cc3c8ddaf2
Samson <samson50wu@gmail.com>
2020-01-13 21:29:34 -0500

    Used CSS grid to display CollegeCards

--- src/components/views/DisplayCampusesView.jsx
+++ src/components/views/DisplayCampusesView.jsx
@@ -2,44 +2,25 @@ import React, { Component } from "react";
 
 import { CampusCard } from "../";
 
+import "../views/DisplayCampusesView.css";
+
 function DisplayCampusesView(props) {
-  // create an array of campus elements / CampusCards
   let campusElements = [];
-  let campusElementsRow = [];
   for (let i = 0; i < props.allCampuses.length; i++) {
-    campusElementsRow.push(
-      <td>
+    campusElements.push(
+      <div class="campus-grid-item">
         <CampusCard
           campus={props.allCampuses[i]}
           buttonFunction={props.buttonFunction}
         />
-      </td>
+      </div>
     );
-    if (i % 3 == 2) {
-      campusElementsRow = [<tr>{campusElementsRow}</tr>];
-      campusElements.push(campusElementsRow);
-      campusElementsRow = [];
-    }
   }
-  if (props.allCampuses.length % 3 !== 2) {
-    campusElementsRow = [<tr>{campusElementsRow}</tr>];
-    campusElements.push(campusElementsRow);
-    campusElementsRow = [];
-  }
-
-  console.log(campusElements);
-
-  // for (let i = 0; i < props.allCampuses.length; i++) {
-  //   campusElements.push(
-
-  //   );
-  // }
 
   return (
     <div>
       <h2>DisplayCampusesView</h2>
-      {campusElements}
-      {/* <img src="/images/campuses/campus01.jpg" alt="campus image" width="500" /> */}
+      <div class="campus-grid-container">{campusElements}</div>
     </div>
   );
 }
