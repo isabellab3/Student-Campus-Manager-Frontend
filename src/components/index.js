1637a28a16a79d52a7b7d1baee6808847f7c34b6
Eva Yan <eyan0749@gmail.com>
2020-01-14 23:51:34 -0500

    Fixed link in CampusCard and started styling SingleCampus view

--- src/components/index.js
+++ src/components/index.js
@@ -23,8 +23,8 @@ export { default as AllStudentsView } from "./views/AllStudentsView.jsx";
 export { default as SingleStudentContainer } from "./containers/SingleStudentContainer.jsx";
 export { default as SingleStudentView } from "./views/SingleStudentView.jsx";
 
-// export { default as SingleCampusContainer } from "./containers/SingleCampusContainer.jsx";
-export { default as SingleCampus } from "./containers/SingleCampus.jsx";
+export { default as SingleCampusContainer } from "./containers/SingleCampusContainer.jsx";
+export { default as SingleCampus } from "./containers/SingleCampusContainer.jsx";
 
 export { default as CampusCard } from "./views/CampusCard.jsx";
 export { default as StudentCard } from "./views/StudentCard.jsx";
